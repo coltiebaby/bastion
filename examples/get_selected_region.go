958f1a3065cd1247d8d5e659de00f13c345c61e1
Colton Robertson <colt.robertson86@gmail.com>
2019-06-27 20:35:48 -0500

    removed links and added an example

---
+++ examples/get_selected_region.go
@@ -0,0 +1,28 @@
+// Shows you how to get the currently selected region
+
+package main
+
+import (
+    "fmt"
+    "net/http/httputil"
+
+    "github.com/coltiebaby/go-lcu/clients/league"
+)
+
+func main() {
+    client, err := league.CreateFromUnix()
+    if err != nil {
+        fmt.Println("Error: ", err)
+        return
+    }
+
+    resp, err := client.Get(`/riotclient/get_region_locale`)
+
+    requestDump, err := httputil.DumpResponse(resp, true)
+    if err != nil {
+        fmt.Println("Error: ", err)
+        return
+    }
+
+    fmt.Println(string(requestDump))
+}
